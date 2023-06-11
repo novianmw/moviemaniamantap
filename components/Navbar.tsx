806a5c6d845a252d80069b9dde7520526f9e30fd
novianmw <novian.mulyoadi.w@gmail.com>
2023-06-11 15:56:58 +0700

    update for react icons/Ri : module not found

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -18,7 +18,7 @@ export default function Navbar() {
                         </label>
                         <div className="relative">
                             <div className="absolute inset-y-0 left-0 px-3 flex items-center">
-                                <RiSearchLine className="w-5 h-5" />
+                               <> <RiSearchLine className="w-5 h-5" /> </>
                             </div>
                             <input
                                 type="search"
@@ -28,7 +28,7 @@ export default function Navbar() {
                                 className="flex border border-black rounded-lg bg-[#188C8D]/10 py-1 md:py-2 pl-10 pr-5 w-full md:w-[300px] lg:w-[300px] shadow-sm"
                             />
                             <button className="absolute inset-y-0 right-0 px-3 flex items-center">
-                                <RiMicFill className="w-5 h-5" />
+                                <> <RiMicFill className="w-5 h-5" /> </>
                             </button>
                         </div>
                     </form>
@@ -65,10 +65,10 @@ export default function Navbar() {
                 </div>
                 <div className="lg:hidden flex flex-row justify-end items-center gap-3">
                     <button className="border text-[14px] border-black py-2 px-4 rounded-lg shadow-lg duration-300 hover:text-white hover:bg-[#E9652A]">
-                        <RiMoonClearFill />
+                        <> <RiMoonClearFill /> </>
                     </button>
                     <button className="border text-[14px] border-black py-2 px-4 rounded-lg shadow-lg duration-300 hover:text-white hover:bg-[#E9652A]">
-                        <RiMenuFill />
+                        <> <RiMenuFill /> </>
                     </button>
                 </div>
             </div>
