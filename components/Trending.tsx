b1eb1c9a80de6d39d2f9c709f070e253e81182ce
novianmw <novian.mulyoadi.w@gmail.com>
2023-09-04 22:26:16 +0700

    update trending: list movie and tv

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -4,96 +4,63 @@ import Image from "next/image"
 import axios from "axios"
 
 export default function Trending() {
-
-    const [trending, setTrending] = useState ([] as any [])
-        useEffect (() => {
-        axios.get (`${process.env.NEXT_PUBLIC_BASE_URL}/trending/movie/week`, {
-            headers: {
-                accept: 'application/json',
-                Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`
-            }
-        }).then((response) => {
-            setTrending(response.data.results)
-        })
+    const [trendingMovies, setTrendingMovies] = useState([] as any[])
+    useEffect(() => {
+        axios
+            .get(`${process.env.NEXT_PUBLIC_BASE_URL}/trending/movie/week`, {
+                headers: {
+                    accept: "application/json",
+                    Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
+                },
+            })
+            .then((response) => {
+                setTrendingMovies(response.data.results)
+            })
     }, [])
 
-    const [trendingTv, setTrendingTv] = useState ([] as any [])
-        useEffect (() => {
-            axios.get (`${process.env.NEXT_PUBLIC_BASE_URL}/discover/tv`, {
+    const [trendingTv, setTrendingTv] = useState([] as any[])
+    useEffect(() => {
+        axios
+            .get(`${process.env.NEXT_PUBLIC_BASE_URL}/discover/tv`, {
                 headers: {
-                    Accept: 'application/json',
-                    Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`
-                }
-            }).then((res) => {
+                    Accept: "application/json",
+                    Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
+                },
+            })
+            .then((res) => {
                 setTrendingTv(res.data.results)
             })
-        }, [])
+    }, [])
 
     return (
-        <section className="w-full flex flex-col items-start">
-            <div className="flex justify-start items-start p-5">
-                <h3 className="font-semibold text-[24px]">
-                    See Trending Movie
-                </h3>
+        <section className="w-full flex flex-col items-start p-6 mx-auto max-w-[1280px]">
+            <div className="flex justify-start items-start ">
+                <h3 className="font-semibold text-[24px]">See Trending Movie</h3>
             </div>
-            <div id="movie-container" className="w-full flex flex-row justify-center items-center gap-5 p-5">
-                {trending.slice(0, 5).map((trend, index) => (
+            <div id="movie-container" className="w-full flex flex-wrap justify-center items-center gap-5 p-5">
+                {trendingMovies.map((trend, index) => (
                     <div key={index} id="movie-wrapper" className="flex flex-col items-center justify-center border-2 border-[#1c2024] p-2">
                         <div id="movie-image" className="flex items-center justify-center">
-                            <Image
-                                src={`${process.env.NEXT_PUBLIC_IMG_URL}${trend.poster_path}`}
-                                width={300}
-                                height={300}
-                                alt="Poster"
-                                priority
-                            />
-                        </div>     
-                        <div id="movie-rating" className="font-normal text-[16px]">
-                            IMDB {trend.vote_average}
-                        </div>
-                        <div id="movie-year" className="font-normal text-[16px]">
-                            {trend.release_date.slice(0,4)}
-                        </div>
-                    </div>
-                ))}
-            </div>
-            <div id="movie-container" className="w-full flex flex-row justify-center items-center gap-5 p-5">
-                {trending.slice(5, 10).map((trend, index) => (
-                    <div key={index} id="movie-wrapper" className="flex flex-col items-center justify-center border-2 border-[#1c2024] p-2">
-                        <div id="movie-image" className="flex items-center justify-center">
-                            <Image
-                                src={`${process.env.NEXT_PUBLIC_IMG_URL}${trend.poster_path}`}
-                                width={300}
-                                height={300}
-                                alt="Poster"
-                                priority
-                            />
+                            <Image src={`${process.env.NEXT_PUBLIC_IMG_URL}${trend.poster_path}`} width={250} height={300} alt="Poster" priority />
                         </div>
                         <div id="movie-rating" className="font-normal text-[16px]">
                             IMDB {trend.vote_average}
                         </div>
                         <div id="movie-year" className="font-normal text-[16px]">
-                            {trend.release_date}
+                            {trend.release_date.slice(0, 4)}
                         </div>
                     </div>
                 ))}
             </div>
+
             <div className="flex justify-start items-start p-5">
-                <h3 className="font-semibold text-[24px]">
-                    See Trending TV
-                </h3>
+                <h3 className="font-semibold text-[24px]">See Trending TV</h3>
             </div>
-            <div id="movie-container" className="w-full flex flex-row justify-center items-center gap-5 p-5">
-                {trendingTv.slice(0, 5).map((trendTv, index) => (
+            <div id="movie-container" className="w-full flex flex-wrap justify-center items-center gap-5 p-5">
+                {trendingTv.map((trendTv, index) => (
                     <div key={index} id="movie-wrapper" className="flex flex-col items-center justify-center border-2 border-[#1c2024] p-2">
                         <div id="movie-image" className="flex items-center justify-center">
-                            <Image
-                                src={`${process.env.NEXT_PUBLIC_IMG_URL}${trendTv.poster_path}`}
-                                width={300}
-                                height={300}
-                                alt="Poster"
-                                priority
-                            />
+                            <Image src={`${process.env.NEXT_PUBLIC_IMG_URL}${trendTv.poster_path}`} width={250} height={300} alt="Poster" priority />
                         </div>
                         <div id="movie-rating" className="font-normal text-[16px]">
                             IMDB {trendTv.vote_average}
