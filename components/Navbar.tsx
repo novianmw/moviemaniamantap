668a11f7f5f9f4ce72f311d2f0eba41029eeb534
novianmw <novian.mulyoadi.w@gmail.com>
2023-09-04 22:23:59 +0700

    Update Navbar: link, toggle, and responsive

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,61 +1,47 @@
+"use client"
 import Link from "next/link"
-import { RiMovie2Fill, RiSearchLine, RiMicFill, RiMenuFill, RiMoonClearFill } from "react-icons/ri"
+import { useState } from "react"
+import { RiMovie2Fill, RiSearchLine, RiMicFill, RiMenuFill, RiMoonClearFill, RiCloseLine } from "react-icons/ri"
 
 export default function Navbar() {
+    const [toggle,setToggle] = useState(true)
+    const handleClick = () => {
+        setToggle((prev) => !prev)
+    }
+
+
     return (
-        <section className="w-full flex flex-col items-start border-b-2 border-[#1c2024]">
-            <div className="w-full flex flex-row justify-between items-center p-6 ">
-                <div className="flex flex-row justify-start items-center">
+        <header className="w-full flex flex-col items-start border-b-2 border-[#1c2024]">
+            <div className="w-full flex flex-row justify-between items-center p-6 mx-auto max-w-[1280px]">
+                <div className="md:w-1/3 flex flex-row justify-start items-center">
                     <Link href="/" className="flex items-center gap-5">
-                        <RiMovie2Fill className="w-8 h-8 lg:w-12 lg:h-12" />
-                        <h1 className="hidden md:flex font-semibold text-[16px] lg:text-[20px]">Movie Mania Mantap</h1>
+                        <RiMovie2Fill className="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12" />
+                        <h1 className="md:flex font-semibold text-[16px] md:text-[22px] lg:text-[24px]">Movie Mania Mantap</h1>
                     </Link>
                 </div>
-                <div className="flex justify-center items-center">
-                    <form method="" action="/">
-                        <label htmlFor="search" className="sr-only">
-                            search
-                        </label>
-                        <div className="relative">
-                            <div className="absolute inset-y-0 left-0 px-3 flex items-center">
-                               <> <RiSearchLine className="w-5 h-5" /> </>
-                            </div>
-                            <input
-                                type="search"
-                                name=""
-                                id=""
-                                placeholder="Search for movies, shows, and other. . ."
-                                className="flex border border-black rounded-lg bg-[#188C8D]/10 py-1 md:py-2 pl-10 pr-5 w-full md:w-[300px] lg:w-[300px] shadow-sm"
-                            />
-                            <button className="absolute inset-y-0 right-0 px-3 flex items-center">
-                                <> <RiMicFill className="w-5 h-5" /> </>
-                            </button>
-                        </div>
-                    </form>
-                </div>
-                <div className="hidden lg:flex flex-row justify-center items-center gap-1">
+                <div className="md:w-1/3 hidden lg:flex flex-row justify-center items-center gap-3">
                     <Link
-                        className="text-[14px] py-2 px-4 duration-300 rounded-lg border-none hover:border-b hover:border-r hover:border-black hover:bg-[#188C8D] hover:text-white hover:rounded-lg hover:shadow-lg"
+                        className="text-[14px] md:text-[16px] py-2 px-4 duration-300 rounded-lg border-none hover:border-b hover:border-r hover:border-black hover:bg-[#188C8D] hover:text-white hover:rounded-lg hover:shadow-lg"
                         href="/">
                         Home
                     </Link>
                     <Link
-                        className="text-[14px] py-2 px-4 duration-300 rounded-lg border-none hover:border-b hover:border-r hover:border-black hover:bg-[#188C8D] hover:text-white hover:rounded-lg hover:shadow-lg"
+                        className="text-[14px] md:text-[16px] py-2 px-4 duration-300 rounded-lg border-none hover:border-b hover:border-r hover:border-black hover:bg-[#188C8D] hover:text-white hover:rounded-lg hover:shadow-lg"
                         href="/movies">
                         Movies
                     </Link>
                     <Link
-                        className="text-[14px] py-2 px-4 duration-300 rounded-lg border-none hover:border-b hover:border-r hover:border-black hover:bg-[#188C8D] hover:text-white hover:rounded-lg hover:shadow-lg"
+                        className="text-[14px] md:text-[16px] py-2 px-4 duration-300 rounded-lg border-none hover:border-b hover:border-r hover:border-black hover:bg-[#188C8D] hover:text-white hover:rounded-lg hover:shadow-lg"
                         href="/tvshows">
                         TV Shows
                     </Link>
                     <Link
-                        className="text-[14px] py-2 px-4 duration-300 rounded-lg border-none hover:border-b hover:border-r hover:border-black hover:bg-[#188C8D] hover:text-white hover:rounded-lg hover:shadow-lg"
-                        href="/explore">
-                        Explore
+                        className="text-[14px] md:text-[16px] py-2 px-4 duration-300 rounded-lg border-none hover:border-b hover:border-r hover:border-black hover:bg-[#188C8D] hover:text-white hover:rounded-lg hover:shadow-lg"
+                        href="/search">
+                        Search
                     </Link>
                 </div>
-                <div className="hidden lg:flex flex-row justify-end items-center gap-3">
+                <div className="md:w-1/3 hidden lg:flex flex-row justify-end items-center gap-3">
                     <button className="border text-[14px] border-black py-2 px-4 rounded-lg shadow-lg duration-300 hover:text-white hover:bg-[#E9652A]">
                         Login
                     </button>
@@ -63,37 +49,49 @@ export default function Navbar() {
                         Sign Up
                     </button>
                 </div>
-                <div className="lg:hidden flex flex-row justify-end items-center gap-3">
-                    <button className="border text-[14px] border-black py-2 px-4 rounded-lg shadow-lg duration-300 hover:text-white hover:bg-[#E9652A]">
+                <div className="lg:hidden flex flex-row justify-end items-center gap-1">
+                    {/* <button className="border text-[14px] border-black py-2 px-4 rounded-lg shadow-lg duration-300 hover:text-white hover:bg-[#E9652A]">
                         <> <RiMoonClearFill /> </>
+                    </button> */}
+                    {/* <button className="border text-[10px] border-black py-2 px-4 rounded-lg shadow-lg duration-300 hover:text-white hover:bg-[#E9652A]">
+                        Login
                     </button>
-                    <button className="border text-[14px] border-black py-2 px-4 rounded-lg shadow-lg duration-300 hover:text-white hover:bg-[#E9652A]">
-                        <> <RiMenuFill /> </>
+                    <button className="border text-[10px] border-black py-2 px-4 rounded-lg shadow-lg duration-300 hover:text-white hover:bg-[#E9652A]">
+                        Sign Up
+                    </button> */}
+                    <button onClick={handleClick} className="border text-[14px] border-black py-2 px-4 rounded-lg shadow-lg duration-300 hover:text-white hover:bg-[#E9652A]">
+                        {toggle ? <RiMenuFill /> : <RiCloseLine/>}
                     </button>
                 </div>
             </div>
-            <div className="lg:hidden flex flex-row items-center gap-2">
+            <div className={`${toggle ? 'hidden' : 'flex lg:hidden'} w-full flex-row justify-center items-center gap-2`}>
                 <Link
-                    className="text-[16px] py-2 px-4 duration-300 rounded-lg border-none hover:border-b hover:border-r hover:border-black hover:bg-[#188C8D] hover:text-white hover:rounded-lg hover:shadow-lg"
+                    className="text-[12px] py-2 px-4 duration-300 rounded-lg border-none hover:border-b hover:border-r hover:border-black hover:bg-[#188C8D] hover:text-white hover:rounded-lg hover:shadow-lg"
                     href="/">
                     Home
                 </Link>
                 <Link
-                    className="text-[16px] py-2 px-4 duration-300 rounded-lg border-none hover:border-b hover:border-r hover:border-black hover:bg-[#188C8D] hover:text-white hover:rounded-lg hover:shadow-lg"
+                    className="text-[12px] py-2 px-4 duration-300 rounded-lg border-none hover:border-b hover:border-r hover:border-black hover:bg-[#188C8D] hover:text-white hover:rounded-lg hover:shadow-lg"
                     href="/movies">
                     Movies
                 </Link>
                 <Link
-                    className="text-[16px] py-2 px-4 duration-300 rounded-lg border-none hover:border-b hover:border-r hover:border-black hover:bg-[#188C8D] hover:text-white hover:rounded-lg hover:shadow-lg"
+                    className="text-[12px] py-2 px-4 duration-300 rounded-lg border-none hover:border-b hover:border-r hover:border-black hover:bg-[#188C8D] hover:text-white hover:rounded-lg hover:shadow-lg"
                     href="/tvshows">
                     TV Shows
                 </Link>
                 <Link
-                    className="text-[16px] py-2 px-4 duration-300 rounded-lg border-none hover:border-b hover:border-r hover:border-black hover:bg-[#188C8D] hover:text-white hover:rounded-lg hover:shadow-lg"
+                    className="text-[12px] py-2 px-4 duration-300 rounded-lg border-none hover:border-b hover:border-r hover:border-black hover:bg-[#188C8D] hover:text-white hover:rounded-lg hover:shadow-lg"
                     href="/explore">
                     Explore
                 </Link>
+                {/* <button className="border text-[14px] border-black py-2 px-4 rounded-lg shadow-lg duration-300 hover:text-white hover:bg-[#E9652A]">
+                        Login
+                    </button>
+                    <button className="border text-[14px] border-black py-2 px-4 rounded-lg shadow-lg duration-300 hover:text-white hover:bg-[#E9652A]">
+                        Sign Up
+                    </button> */}
             </div>
-        </section>
+        </header>
     )
 }
